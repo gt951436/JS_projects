afc168b5b720209a5f5155c5d412d209da93e6fd
Garv Tyagi <128500987+gt951436@users.noreply.github.com>
2024-08-09 13:12:52 +0000

    data types JS

---
+++ js_basics/01_data_types.js
@@ -0,0 +1,16 @@
+const accountId = 134265
+let emailId = "gt@gmail.com"
+var accountPassword = "12345"
+accountCity = "jaipur"
+
+//accountId=2
+emailId = "gt95@gmail.com"
+accountPassword = "34213"
+accountCity = "Delhi"
+let accountState;
+
+/*
+prefer not to use var keyword while declaring variables
+due to block scope and functinal scope issues
+*/
+console.table([accountId, emailId, accountPassword, accountCity,accountState])
---
+++ js_basics/02_data_types.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all JS code as newer version
+/*
+use strict likhne ki need nahi hai automatically sab
+new version me hojata hai.
+*/
+// alert(3+3) //icorrect for browser not for Node.
+
+let Name = "garv";  // string
+let age = 20;  // number
+let isLoggedIn = true; // boolean
+
+//number --> 2^53 something (range)
+// bigint
+// string --> "gewrgerby"
+// boolean --> true/false
+// null --> stadalone value
+// undefined --> value not assigned
+// symbol --> uniqueness ke baare mein baat krni hai to use krenge
+
+// object---> complex data type
+
+// typeof operator --> return the data type of a variable 
+
+//typeof null --> object  // weird?  --> null is an object!!
+// typeof undefined --> undefined
+
+console.log(typeof undefined)
