25c4d131ff0740c94907d173618cf5c75cf77980
Garv Tyagi <128500987+gt951436@users.noreply.github.com>
2024-08-10 07:46:36 +0000

    Type conversion changes

---
+++ js_basics/03_conversionOperations.js
@@ -0,0 +1,36 @@
+let score = "33" // string gets converted to number when Number(score)
+//let score ="33ab"  // string
+//let score = null
+//let score = undefined // typt-->undefined
+//let score =true
+//let score = "garv"
+
+// console.log(typeof (score))
+
+let valueInNumber = Number(score)  // valueInNumber = 0 for score = null
+
+// console.log(typeof (valueInNumber))
+// console.log(valueInNumber) 
+
+// "33" -->33
+// "33abc" --> NaN
+// true --> 1 (false-->0)
+
+//---------------------------------------------------------------------------------
+
+//let isLoggedIn = 1
+//let isLoggedIn = ""  // Empty string("") --> false (on Boolean conversion of string)
+let isLoggedIn = "garv"  // Non-empty string --> true ( on Boolean conversion of string)
+let boolIsLoggedIn = Boolean(isLoggedIn)
+//console.log(typeof(boolIsLoggedIn))
+//console.log(boolIsLoggedIn)
+
+//-------------------------------------------------------------------------------------
+
+//let num = false   // false -->string
+let num = undefined  // undefined --> string
+//let num = null  // null --> string
+
+let stringNum = String(num)
+console.log(typeof(stringNum))
+console.log(stringNum)
