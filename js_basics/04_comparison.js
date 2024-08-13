ae45cae67053a218a2c84586c668af9cf8a309c5
Garv Tyagi <128500987+gt951436@users.noreply.github.com>
2024-08-13 04:04:37 +0000

    Data Types summary

--- js_basics/04_comparison.js
+++ js_basics/04_comparison.js
@@ -26,3 +26,27 @@
 // STRICT CHECK: === -->checks values as well as data types
 //console.log("2"===2) //false
 
+//-------------------------------------------------------------------------------------
+//DATA TYPES
+
+//PRIMITIVE : String,Number,Boolean,null,undefined,Symbol,BigInt
+const id = Symbol('123')
+const anotherId = Symbol("123")
+//console.log(id === anotherId)
+
+const bigNumber = 2526246735392848n //BigInt
+//NON-PRIMITIVE(Reference type): in memeory,reference can be allocated directly
+//Array,Objects,Functions
+
+const heroes = ["shaktiman","naagraj","doga"]  //Array
+
+let myObj={ // Object
+    name:"Garv",
+    age :20,
+}
+
+const myFun = function(){
+    console.log("Hellew Duniya!")
+    
+}
+console.log(typeof myFun)  //function type
